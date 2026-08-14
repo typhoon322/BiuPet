1f86d1844287364c43516fb4093c3308f0dc92d7
YanX <typhoon322@qq.com>
2026-08-14 23:26:27 +0800

    chore: drop unused GB2312 table/conversion (font is unicode-keyed)

--- tests/text_convert_test.cpp
+++ tests/text_convert_test.cpp
@@ -26,27 +26,6 @@ int main() {
     assert(utf8::truncate_by_width(s, 10, 6, 12) == 1);
     assert(utf8::truncate_by_width(s, 1000, 6, 12) == 1 + 3 + 3);
 
-    // 你 = U+4F60 = GB2312 0xC4E3 ; 好 = U+597D = GB2312 0xBAC3
-    {
-        char out[32];
-        size_t n = utf8_to_gb2312("\xE4\xBD\xA0\xE5\xA5\xBD", out, sizeof(out), 1000, 6, 12);
-        assert(n == 4);
-        assert((unsigned char)out[0] == 0xC4 && (unsigned char)out[1] == 0xE3);
-        assert((unsigned char)out[2] == 0xBA && (unsigned char)out[3] == 0xC3);
-    }
-    // ASCII 透传
-    {
-        char out[32];
-        size_t n = utf8_to_gb2312("task A", out, sizeof(out), 1000, 6, 12);
-        assert(n == 6 && out[0] == 't' && out[5] == 'A');
-    }
-    // 未映射字符 -> '?'
-    {
-        char out[32];
-        size_t n = utf8_to_gb2312("\xF0\x9F\x98\x80", out, sizeof(out), 1000, 6, 12); // 😀 不在 GB2312
-        assert(n == 1 && out[0] == '?');
-    }
-
     std::puts("ALL text_convert TESTS PASS");
     return 0;
 }
